ce258330be748d006ca61ad0e1e3199fa0ae269c
xigumalu <sylvia.schroeder@ucl.ac.uk>
2016-02-09 17:17:00 +0000

    Scale image to range of presented pixels (not whole image)

--- redraw_meanimg.m
+++ redraw_meanimg.m
@@ -2,12 +2,14 @@
 function redraw_meanimg(h)
 
 I = h.dat.mimg(:,:,h.dat.map);
+Irange = I(max(1,h.dat.ylim(1)):min(size(I,1),h.dat.ylim(2)), ...
+    max(1,h.dat.xlim(1)):min(size(I,1),h.dat.xlim(2)));
 
-axes(h.axes2); imagesc(I);
+axes(h.axes2); imagesc(I, [min(Irange(:)) max(Irange(:))]);
 xlim([h.dat.xlim]); ylim([h.dat.ylim]);
 axis off
 colormap('gray')
-axes(h.axes3); imagesc(I);
+axes(h.axes3); imagesc(I, [min(Irange(:)) max(Irange(:))]);
 xlim([h.dat.xlim]); ylim([h.dat.ylim]);
 axis off
 colormap('gray')
